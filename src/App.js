e1e0fc7a6a5d639f9d55f3718d22f86f3d7e8e7a
Rebecca <bsoza93@gmail.com>
2022-09-30 18:23:24 -0500

    added lenovo comp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import lenovo from "./lenovo.png"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Parallax pages={3} style={{ top: "0", left: "0" }}>
+          <ParallaxLayer
+            offset={0}
+            speed={1}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <p>Scroll down</p>
+            <img alt="workComp" clasName="computer" src={lenovo}  />
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={1}
+            speed={2}
+            style={{ backgroundColor: "#ff6d6d" }}
+          />
+
+          <ParallaxLayer
+            offset={1}
+            speed={0.5}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "white",
+            }}
+          >
+            <p>JK</p>
+          </ParallaxLayer>
+        </Parallax>
       </header>
     </div>
   );
